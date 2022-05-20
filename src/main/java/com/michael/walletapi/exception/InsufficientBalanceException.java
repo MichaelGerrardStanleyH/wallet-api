f63da3b13dc4647ae4e3819c0d6d97a8161e8c1f
MichaelGerrardStanleyH <hutagalung.michael23@gmail.com>
2022-05-20 19:05:18 +0700

    add Dockerfile

--- src/main/java/com/michael/walletapi/exception/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.michael.walletapi.exception;
-
-public class InsufficientBalanceException extends Exception{
-    public InsufficientBalanceException(String str){
-        super(str);
-    }
-}
